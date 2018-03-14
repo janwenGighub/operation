3c4be1d25632314bb3b09e0d71064e3e7508c042
皮皮虾 <1164698177@qq.com>
2018-03-14 23:56:47 +0800

    公共底部tabbar设置完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <!-- tabbar star -->
+    <div class="weui-tabbar">
+        <router-link to="" class="weui-tabbar__item weui-bar__item_on">
+            <span style="display: inline-block;position: relative;">
+                <!-- <span class="weui-badge" style="position: absolute;top: -2px;right: -13px;">8</span> -->
+            </span>
+            <p class="weui-tabbar__label">操作指南</p>
+        </router-link>
+        <a to="" class="weui-tabbar__item">
+            <p class="weui-tabbar__label">视频</p>
+        </a>
+    </div>
+    <!-- tabbar end -->
   </div>
 </template>
 
@@ -13,4 +25,17 @@ export default {
 
 <style>
 @import '../static/css/reset.css';
+.weui-tabbar{
+  position: fixed;
+}
+.weui-tabbar__item{
+  padding: 0;
+}
+.weui-tabbar__label{
+  height: 57px;
+  line-height: 57px;
+  font-size: .875rem;
+  color: black;
+  font-weight: bolder;
+}
 </style>
