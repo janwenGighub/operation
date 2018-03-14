6cac343d37c5a45363996e59922934000e67257a
皮皮虾 <1164698177@qq.com>
2018-03-15 00:27:49 +0800

    DetailsList中详情列表设置完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // import App from '@/App'
-import GuideList from '../components/guide/GuideList.vue' // 类别指导列表
+import GuideList from '../components/guide/GuideList.vue' // 分类指导列表
 import DetailsList from '../components/guide/DetailsList.vue' // 指导详情列表
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', component: GuideList }, // 类别指导列表
+    { path: '/', component: GuideList }, // 分类指导列表
     { path: '/detailsList', component: DetailsList } // 指导详情列表
   ]
 })
