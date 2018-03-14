1eaed0d33c2c1761c9441d8ffcdd12dcca6674c9
皮皮虾 <1164698177@qq.com>
2018-03-15 00:14:09 +0800

    GuideList操作指南中的类别列表设置完成

---
+++ src/components/guide/GuideList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <div class="weui-cells">
+        <router-link class="weui-cell weui-cell_access" to="/">
+            <div class="weui-cell__bd">
+                <p>功能类应用指导</p>
+            </div>
+            <span class="weui-badge" style="position: absolute;top: 14;right: 13px;">8</span>
+        </router-link>
+        <a class="weui-cell weui-cell_access" href="javascript:;">
+            <div class="weui-cell__bd">
+                <p>学习类应用指导</p>
+            </div>
+            <span class="weui-badge" style="position: absolute;top: 14px;right: 13px;">8</span>
+        </a>
+        <a class="weui-cell weui-cell_access" href="javascript:;">
+            <div class="weui-cell__bd">
+                <p>学习类应用指导</p>
+            </div>
+            <span class="weui-badge" style="position: absolute;top: 14px;right: 13px;">8</span>
+        </a>
+        <a class="weui-cell weui-cell_access" href="javascript:;">
+            <div class="weui-cell__bd">
+                <p>学习类应用指导</p>
+            </div>
+            <span class="weui-badge" style="position: absolute;top: 14px;right: 13px;">8</span>
+        </a>
+        <a class="weui-cell weui-cell_access" href="javascript:;">
+            <div class="weui-cell__bd">
+                <p>学习类应用指导</p>
+            </div>
+            <span class="weui-badge" style="position: absolute;top: 14px;right: 13px;">8</span>
+        </a>
+    </div>
+  </div>
+</template>
+<style>
+.weui-cells{
+    margin-top: 0;
+}
+.weui-cell_access{
+    font-size: 1rem;
+    color: black;
+    font-weight: bolder;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // import App from '@/App'
+import GuideList from '../components/guide/GuideList.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    { path: '/', component: GuideList }
   ]
 })
